12bb6e744d94dfbbc8a122cc5e15ec28dfd19f3c
Sasukexo <aswinash1002@gmail.com>
2026-02-07 18:26:26 +0530

    added the link to backend logic

--- Front-end/migration-ui/src/services/api.ts
+++ Front-end/migration-ui/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://orthodox-marie-jeanne-aswinxo-b6a366c1.koyeb.app",
 });
 
 export const uploadCSV = async (file: File) => {
